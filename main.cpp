ef4703d206df3defedbc4be2d0257a5cd2becea6
Zac Colbert <zcolbert1993@gmail.com>
2021-11-01 23:55:24 -0700

    Add comments

--- main.cpp
+++ main.cpp
@@ -22,10 +22,10 @@
 
 #define SCREEN_WIDTH 640 
 #define SCREEN_HEIGHT 480
+
 #define MIN_VALUE 10
 #define MAX_VALUE 100
 #define NUM_VALUES 100
-
 
 
 int main(int argc, char** argv)
@@ -33,25 +33,22 @@ int main(int argc, char** argv)
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
 
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        return 1;
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) 
+    {
+        return -1;
     }
     else {
         window = SDL_CreateWindow(
-            "Algorithm Visualizer", 
-            SDL_WINDOWPOS_UNDEFINED, 
-            SDL_WINDOWPOS_UNDEFINED, 
-            SCREEN_WIDTH, 
-            SCREEN_HEIGHT, 
-            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+            "Algorithm Visualizer",                 // Window title
+            SDL_WINDOWPOS_UNDEFINED,                // Initial X position
+            SDL_WINDOWPOS_UNDEFINED,                // Initial Y postion
+            SCREEN_WIDTH,                           // Window width
+            SCREEN_HEIGHT,                          // Window height
+            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN    // Rendering flags
         );
         renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     }
 
-    int values[NUM_VALUES];
-    populate_array(values, NUM_VALUES, 1, 100);
-
-    Data data(values, NUM_VALUES);
 
     SDL_Color colors[NUM_VALUES];
     for (int i=0; i<NUM_VALUES; ++i)
@@ -70,23 +67,24 @@ int main(int argc, char** argv)
     screen_rect.w = SCREEN_WIDTH;
     screen_rect.h = SCREEN_HEIGHT;
 
+    int values[NUM_VALUES];
+    Data data(values, NUM_VALUES);
+
     Visualizer v(renderer, screen_rect, values, colors, NUM_VALUES);
 
     Observer o(&v);
     data.attach(&o, ACCESSED);
     data.attach(&o, MODIFIED);
 
-
-
     bool quit = false;
     SDL_Event e;
     while (!quit) 
     {
         populate_array(values, NUM_VALUES, 1, 100);
-        v.redraw();
-        bubblesort(data);
-        //mergesort(data, 0, NUM_VALUES);
-        v.redraw();
+
+        v.redraw();         // draw the initial data
+        bubblesort(data);   // sort and draw each step
+        v.redraw();         // draw the sorted data
 
         while (SDL_PollEvent(&e) != 0)
         {
